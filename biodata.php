773b35fa12bd97279046c5e89adf0e1a43590753
DiazMunggaran <227006087@student.unsil.ac.id>
2023-09-29 19:19:29 +0700

    add biodata

---
+++ biodata.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <fieldset style="width: 500px;" margin="auto;">
+        <legend>Bio Data</legend>
+    <table cellpadding="10" cellspacing="0">
+            <tr>
+                <td>Nama</td><td>:</td>
+                <td>Diaz R</td>
+            </tr>
+            <tr>
+                <td>Asal</td><td>:</td>
+                <td>Ciamis</td>
+            </tr>
+            <tr>
+                <td>Pekerjaan</td><td>:</td>
+                <td>Mahasiswa</td>
+            </tr>
+            <tr>
+                <td>Jurusan</td><td>:</td>
+                <td>Informatika</td>
+            </tr>
+            <tr>
+                <td>Faktultas</td><td>:</td>
+                <td>Teknik</td>
+            </tr>
+            <tr>
+                <td>Hobi</td><td>:</td>
+                <td>Futsal, Gitar</td>
+            </tr>
+        </table>
+    </fieldset>
+    </body>
+    </html>
